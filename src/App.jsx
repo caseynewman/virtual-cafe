88dcb9202c45f8137953bdd135bc5598a9f7025a
Yogi Spice <caseynnewman@yahoo.com>
2024-12-17 14:23:46 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Virtual Cafe</h1>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </>
+  )
+}
+
+export default App
