d118da594783a608db1a550c62eccd2f63f5082e
Yogi Spice <caseynnewman@yahoo.com>
2024-12-23 14:21:29 -0500

    creating pomodor timer and initial settings

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -1,3 +1,22 @@
 // pomodoro style timer
 // counts down
 // 25 minute work, or 5-10 minute break?
+
+import { useState } from "react";
+
+export default function PomodoroTimer() {
+    const WORK_TIME = 25 * 60;
+    const BREAK_TIME = 5 * 60;
+
+    const [timeLeft, setTimeLeft] = useState(WORK_TIME);
+    const [isActive, setIsActive] = useState(false);
+    const [isWorkSession, setIsWorkSession] = useState(true);
+
+    
+
+    return (
+        <>
+        
+        </>
+    )
+}
